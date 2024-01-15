bb813b8520c0f31715c05f6fc3004e621cf18752
Ariel Flahaut <111144387+arielflt@users.noreply.github.com>
2024-01-15 11:01:10 +0100

    fix

---
+++ creatingdataset_v1.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import torch
+from torch_geometric.data import Data
+import os
+
+def load_and_process_data(prefix):
+
+    node_file = f'apo/{prefix}_nodes.csv'
+    link_file = f'apo/{prefix}_links.csv'
+
+    node_data = pd.read_csv(node_file)
+    link_data = pd.read_csv(link_file)
+
+
+    node_features = node_data[['atom_type', 'residue_type', 'center_x', 'center_y', 'center_z', 'radius', 'voromqa_sas_potential', 'residue_mean_sas_potential', 'residue_sum_sas_potential', 'residue_size', 'sas_area', 'solvdir_x', 'solvdir_y', 'solvdir_z', 'voromqa_sas_energy', 'voromqa_depth', 'voromqa_score_a', 'voromqa_score_r', 'volume', 'volume_vdw', 'ufsr_a1', 'ufsr_a2', 'ufsr_c2', 'ufsr_c3', 'ev28', 'ev56', 'ground_truth']]
+    link_features = link_data[['atom_index1', 'atom_index2', 'area', 'boundary', 'distance', 'voromqa_energy', 'seq_sep_class', 'covalent_bond', 'hbond']]
+
+
+    edge_index = torch.tensor([link_features['atom_index1'].values, link_features['atom_index2'].values], dtype=torch.long)
+
+
+    self_links = torch.arange(0, len(node_features))
+    edge_index = torch.cat([edge_index, torch.stack([self_links, self_links])], dim=1)
+    edge_index = torch.cat([edge_index, edge_index[[1, 0], :]], dim=1)  # Add reverse direction
+
+
+    node_features_tensor = torch.tensor(node_features.values, dtype=torch.float)
+
+
+    data = Data(x=node_features_tensor, edge_index=edge_index)
+
+    return data
+
+
+candidate_pairs_file = 'apo/candidate_pairs.txt'
+candidate_pairs = pd.read_csv(candidate_pairs_file, delim_whitespace=True)
+
+
+graphs = {}
+for index, row in candidate_pairs.iterrows():
+    prefix = f"{row['apo_pdb_id']}_{row['apo_chain_id']}"
+    graphs[prefix] = load_and_process_data(prefix)
+
+
+
+save_dir = 'saved_graphs_1'
+os.makedirs(save_dir, exist_ok=True)
+
+
+for prefix, graph in graphs.items():
+    save_path = os.path.join(save_dir, f'{prefix}_graph.pt')
+    torch.save(graph, save_path)
+
+print(f"All graphs have been saved in the directory: {save_dir}")
